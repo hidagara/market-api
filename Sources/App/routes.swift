4be7703e0c222c9a4b4c9cba68a4404d2e8936a8
hidagara <hidagara@gmail.com>
2019-12-11 09:29:20 +0300

    some more methods

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -17,13 +17,31 @@ public func routes(_ router: Router) throws {
         return InfoResponse(request: data)
         
     }
+    
+    router.get("date") { req -> String in
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMMM YYYY"
+        return dateFormatter.string(from: Date())
+    }
+    
+    router.get("counter", Int.parameter) { req -> CountJSON in
+        let count = try req.parameters.next(Int.self)
+        return CountJSON(count: count)
+    }
+    
+    router.post("user-info") { req -> String in
+        let userInfo = try req.content.syncDecode(UserInfoData.self)
+        return "Hello \(userInfo.name) your age is \(userInfo.age)"
+    }
 
     // Example of configuring a controller
     let todoController = TodoController()
-//    let storeController = StoreController()
-//    router.get("todos", use: StoreController.storeList(storeController))
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+}
+
+struct Counter: Content {
+    let number: Int
 }
 
 struct InfoData: Content {
@@ -32,4 +50,13 @@ struct InfoData: Content {
 
 struct InfoResponse: Content {
     let request: InfoData
+}
+
+struct CountJSON: Content {
+    let count: Int
+}
+
+struct UserInfoData: Content {
+    let name: String
+    let age: Int
 }
