bec3470c9ace4b11bee8376c36ac35d36be8d0e4
hidagara <hidagara@gmail.com>
2019-12-11 13:52:57 +0300

    add store model

--- Sources/App/Models/Store.swift
+++ Sources/App/Models/Store.swift
@@ -2,16 +2,30 @@ import FluentSQLite
 import Vapor
 
 /// A single entry of a Todo list.
-final class Store {
-    /// The unique identifier for this `Todo`.
+final class Store: Codable {
+    /// The unique identifier for this `Store`.
     var id: Int?
 
-    /// A title describing what this `Todo` entails.
+    /// A title describing what this `Store` entails.
     var title: String
 
-    /// Creates a new `Todo`.
+    /// Creates a new `Store`.
     init(id: Int? = nil, title: String) {
         self.id = id
         self.title = title
     }
 }
+//
+//extension Store: Model {
+//    typealias Database = SQLiteDatabase
+//
+//    typealias ID = Int
+//
+//    static let idKey: IDKey = \Store.id
+//
+//
+//}
+
+extension Store: SQLiteModel {}
+extension Store: Content {}
+extension Store: Migration {}
